0b56049fad8b8b93da2b8b109458213d7cf6628c
Square-Face <linus@michelsson.se>
2021-09-05 00:42:41 +0200

    added database models and started working on templates

--- To_Do/backend/models.py
+++ To_Do/backend/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=256)
+    password = models.CharField(max_length=64)
+    created_at = models.DateTimeField(default=datetime.utcnow)
+
+    def __str__(self):
+        return str(self.username)
+
+class Items(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    state = models.CharField(max_length=128)
+    created_at = models.DateTimeField(default=datetime.utcnow)
+
+    def __str__(self):
+        return self.name
